376e28a17b4c1f0a7285df4643a73f21f0b42e96
skald1311 <hoangmanhduong01@gmail.com>
2024-06-19 20:54:53 -0600

    feat: handle errors

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from "date-fns";
 import { supabase } from "./supabase";
+import { notFound } from "next/navigation";
 
 /////////////
 // GET
@@ -11,11 +12,10 @@ export async function getCabin(id) {
     .eq("id", id)
     .single();
 
-  // For testing
-  // await new Promise((res) => setTimeout(res, 1000));
-
   if (error) {
     console.error(error);
+    // Manually call not found to handle unknown [cabinId] route
+    notFound();
   }
 
   return data;
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,13 +1,12 @@
-import CabinCard from "@/app/_components/CabinCard";
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabins",
 };
 
 export default function Page() {
-  // CHANGE
-  const cabins = [];
-
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -21,14 +20,9 @@ export default function Page() {
         away from home. The perfect spot for a peaceful, calm vacation. Welcome
         to paradise.
       </p>
-
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList /> {/*Move into own component to use suspense */}
+      </Suspense>
     </div>
   );
 }
