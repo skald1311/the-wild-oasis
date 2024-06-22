e601ffbe3a95c583f95e04381c5844e4d7ca46b9
skald1311 <hoangmanhduong01@gmail.com>
2024-06-22 01:11:29 -0600

    feat: add guestId to session

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -12,6 +13,23 @@ const authConfig = {
     authorized({ auth, request }) {
       // Return either true or false. True -> authorized, False -> not authorized
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      // Calls after the user provided info to log in but before actually logging in
+      try {
+        const existingGuest = await getGuest(user.email);
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
