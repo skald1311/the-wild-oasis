2259d04b775e73344787e221c3b4404a8b349338
skald1311 <hoangmanhduong01@gmail.com>
2025-12-02 19:10:35 -0700

    conflict generated
    # Conflicts:
    #	test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ def main():
     print("helolo world")
     print("this is main changes.")
     print("testBranch changes")
+    print("rebase test test test")
 
 if __name__ == '__main__':
     main()
