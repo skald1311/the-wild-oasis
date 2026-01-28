10683b1a0dc10c6738d212c4e1caa5447d815cdd
skald1311 <hoangmanhduong01@gmail.com>
2026-01-28 14:23:07 -0700

    first

--- test.py
+++ test.py
@@ -3,13 +3,19 @@ def main():
     print("this is main changes.")
     print("testBranch changes")
     print("rebase test test test")
-    print("alalalallala")
+    print("lalalalal fix")
+
+    print("aahahaha")
 
 def add(x, y):
+    if (x +  y == 0):
+        print("it's zero")
+        print("ahahahahha test")
     return x + y
 
 def subtract(a, b):
     print(f"Subtracting {a} and {b}: ")
+    print(a, b)
     return a - b
 
 def multiply(a, b):
