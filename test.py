c38f2ce599e127805ee23858aec83de854d122fd
skald1311 <hoangmanhduong01@gmail.com>
2026-01-28 14:26:30 -0700

    someone committed

--- test.py
+++ test.py
@@ -9,7 +9,13 @@ def add(x, y):
     return x + y
 
 def subtract(a, b):
+    print(f"Subtracting {a} and {b}: ")
     return a - b
+
+def multiply(a, b):
+    return a * b
+
+print("12312341234123")
 
 if __name__ == '__main__':
     main()
